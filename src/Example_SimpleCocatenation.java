fc73628e4e62161ea701e99780f7c53e06a11ed6
emileqae <emileesapa@gmail.com>
2022-02-27 17:39:25 -0600

    initial commit

---
+++ src/Example_SimpleCocatenation.java
@@ -0,0 +1,12 @@
+
+public class Example_SimpleCocatenation {
+
+	public static void main(String[] args) {
+		
+		int myVariable1 = 20;
+		String myVariable2 = "Hello World";
+		
+		System.out.println(myVariable1+" "+myVariable2);
+	}
+
+}
---
+++ src/pro/tech/alliance/sqa/projects/Nested_If_Else.java
@@ -0,0 +1,18 @@
+package pro.tech.alliance.sqa.projects;
+
+public class Nested_If_Else {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
